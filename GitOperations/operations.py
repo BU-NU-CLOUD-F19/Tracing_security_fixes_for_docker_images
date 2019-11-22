e95dc5ede03990587f1b0b47034aba410fa16dbb
Akshat Shukla <shukla.ak@husky.neu.edu>
2019-11-22 00:56:36 -0500

    Modified code to generate reports for visualization

--- GitOperations/operations.py
+++ GitOperations/operations.py
@@ -3,12 +3,12 @@ from github import Github
 import click
 from collections import defaultdict
 import string
-import urllib
-import requests
+from tqdm import tqdm
+import pandas as pd
 from connect_postgres import PostgresOps
-from bs4 import BeautifulSoup
 from analyze_vulnerability_timestamps import AnalyzeTimeStamps
 from datetime import datetime
+
 
 class GithubCli:
     def __init__(self, username, password, api_key):
@@ -81,7 +81,7 @@ class GithubCli:
                 words = line.split(" ")
                 # Remove words in all-caps and words starting with punctuation from the current line
                 for word in words:
-                    if word.isupper() or (any(word.startswith(punct) for punct in string.punctuation)):
+                    if word.isupper() or "'" in word or (any(word.startswith(punct) for punct in string.punctuation)):
                         line = line.replace(word, "")
                 line = line.strip()
                 # Add the clean line to the list of packages
@@ -99,7 +99,7 @@ class GithubCli:
         g = Github(self.username, self.password)
 
         # Fetch repositories
-        repo_list = open('repo_list_mock.txt', 'r')
+        repo_list = open('repo_list.txt', 'r')
 
         dockerfile_packages_map = defaultdict(list)
 
@@ -149,7 +149,6 @@ def main(username, password, api_key):
     :param api_key: XForce API Key
     """
     cli_obj = GithubCli(username, password, api_key)
-    analyze_ts = AnalyzeTimeStamps(cli_obj.api_key)
 
     dockerfiles = cli_obj.fetch_dockerfiles()
     packages = []
@@ -157,44 +156,52 @@ def main(username, password, api_key):
         if dockerfiles[image_name]:
             packages.append(dockerfiles[image_name])
     packages = [j for i in packages for j in i]
+    packages = list(set(packages))
 
+    print("Fetching information for", len(packages), "packages.")
     postgres_obj = PostgresOps(packages)
     postgres_obj.connect()
     clair_db_reports = postgres_obj.get_clair_reports()
-
-    # clair_db_reports['Date_Reported'] = clair_db_reports.apply(lambda r:
-    #                                                            analyze_ts.fetch_xforce_timestamp(r['Vulnerability']),
-    #                                                            axis=1)
-    # clair_db_reports['Date_Fixed'] = clair_db_reports.apply(lambda row:
-    #                                                         analyze_ts.fetch_version_timestamp(row['OS'],
-    #                                                                                            row['Package'],
-    #                                                                                            row['Package_Version']),
-    #                                                         axis=0)
-    # date_format = "%Y-%m-%d"
-    # clair_db_reports['Days_For_Fix'] = clair_db_reports(lambda row:
-    #                                                     abs((datetime.strptime(row['Date_Fixed'],
-    #                                                                           date_format) - datetime.strptime(row['Date_Reported'],
-    #                                                                                                            date_format)).days),
-    #                                                         axis=0)
-    #
-    """
-    for i, report in clair_db_reports.iterrows():
-        cve_no = report['Vulnerability']
-        pkg_name = report['Package']
-        os_name = report['OS']
-        pkg_version = report['Package_Version']
-
-        date_reported = analyze_ts.fetch_xforce_timestamp(cve_no)
-        date_fixed = analyze_ts.fetch_version_timestamp(os_name, pkg_name, pkg_version)
-
-        print("Reported:", date_reported)
-        print("Fixed:", date_fixed)
-
-        days_to_fix = datetime.strptime(date_fixed, "%Y-%m-%d") - datetime.strptime(date_reported, "%Y-%m-%d")
-    
-    """
     clair_db_reports.to_csv("report.csv", index=False)
 
 
+@click.command()
+@click.option('--api_key', prompt='XForce API Key', hide_input=True, help='XForce API Key')
+def analyze_date_diff(api_key):
+    tqdm.pandas()
+    report_filename = "Dedup_Report.csv"
+    data = pd.read_csv(report_filename)
+    analyze_ts = AnalyzeTimeStamps(api_key)
+
+    data['Date_Reported'] = data.progress_apply(lambda r:
+                                                analyze_ts.fetch_xforce_timestamp(r['Vulnerability']),
+                                                axis=1)
+    data['Date_Fixed'] = data.progress_apply(lambda row:
+                                             analyze_ts.fetch_version_timestamp(row['OS'],
+                                                                                row['Package'],
+                                                                                row['Package_Version']),
+                                             axis=1)
+
+    def get_date_diff(row):
+        date_format = "%Y-%m-%d"
+        if row['Date_Fixed']:
+            fixed = datetime.strptime(row['Date_Fixed'], date_format)
+        else:
+            fixed = None
+        if row['Date_Reported']:
+            reported = datetime.strptime(row['Date_Reported'], date_format)
+        else:
+            reported = None
+        if fixed and reported:
+            return abs((fixed - reported).days)
+        else:
+            return None
+
+    data['Days_For_Fix'] = data.progress_apply(lambda row: get_date_diff(row), axis=1)
+
+    data.to_csv('Final_Report.csv')
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    analyze_date_diff()
